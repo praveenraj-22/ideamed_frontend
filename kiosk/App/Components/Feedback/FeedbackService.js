653baf94c892ac97542cd563e85bcd4973b53780
praveenraj <praveenraj.yv@gmail.com>
2018-11-20 13:38:59 +0530

    feedbackservice.js
    update

--- kiosk/App/Components/Feedback/FeedbackService.js
+++ kiosk/App/Components/Feedback/FeedbackService.js
@@ -3,7 +3,7 @@ var FeedbackModule=angular.module('FeedbackModule',[])
 FeedbackModule.service('FeedbackService',['REST_URI','$http',function(REST_URI,$http){
     this.register=function(feedback){                               /** this.register-> function of ng-click */
         console.log('content reached to register service');
-        console.log(REST_URI);
+        
     return  $http.post(REST_URI +'/registerFeedback',feedback)
     .then(
         function(response){
