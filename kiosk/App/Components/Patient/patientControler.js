a441e616c94973a06997fb05a26e4cc58006eaba
praveenraj <praveenraj.yv@gmail.com>
2018-12-18 16:38:08 +0530

    upload

--- kiosk/App/Components/Patient/patientControler.js
+++ kiosk/App/Components/Patient/patientControler.js
@@ -10,23 +10,23 @@ app.controller('patientControler', ['patientservice', '$location', '$scope', '$r
 
 
 
-        console.log('content reached to patient Controller');
-        console.log("From date : " + $scope.dateAD.fdatead);
-        console.log("To Date : " + $scope.dateAD.tdatead);
-        console.log("Mobile number :"+$scope.dateAD.mobile);
+        // console.log('content reached to patient Controller');
+        // console.log("From date : " + $scope.dateAD.fdatead);
+        // console.log("To Date : " + $scope.dateAD.tdatead);
+        // console.log("Mobile number :"+$scope.dateAD.mobile);
 
 
         if ($scope.dateAD.tdatead == null) {
 
-            alert("please select To Date")
+          //  alert("please select To Date")
             $location.path('/patient')
         } else if ($scope.dateAD.fdatead == null) {
-            alert("please select From Date")
+        //    alert("please select From Date")
             $location.path('/patient')
         }
 else if($scope.dateAD.mobile==null)
 {
-    alert($scope.dateAD.mobile);
+  //  alert($scope.dateAD.mobile);
     $scope.feedback.pmobile=0;
     patientservice.datead($scope.dateAD.fdatead, $scope.dateAD.tdatead, $scope.dateAD.mobile, $scope.dateAD).then(function (response) {
         console.log(response)
@@ -43,8 +43,8 @@ else if($scope.dateAD.mobile=="")
 {
     $scope.dateAD.mobile=0;
     $scope.feedback.pmobile=0;
-     alert($scope.dateAD.mobile);
-     alert($scope.feedback.pmobile);
+    //  alert($scope.dateAD.mobile);
+    //  alert($scope.feedback.pmobile);
     patientservice.datead($scope.dateAD.fdatead, $scope.dateAD.tdatead, $scope.dateAD.mobile, $scope.dateAD).then(function (response) {
         console.log(response)
   
