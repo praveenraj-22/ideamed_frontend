5946937d568d3247067fef400fc8d8db4b6193c7
praveenraj <praveenraj.yv@gmail.com>
2018-12-25 21:20:15 +0530

    update

--- kiosk/App/Components/DashBoard/DashboardController1.js
+++
@@ -1,7 +0,0 @@
-app.controller('DashboardController', ['DashboardService', '$location', '$scope','$rootScope','$cookieStore','$routeParams', function (DashboardService, $location, $scope,$rootScope,$cookieStore,$routeParams) 
-{
-
-    
-    
-
-}]);
